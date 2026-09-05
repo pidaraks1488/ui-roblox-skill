e554a648adf40fa519fc3acc9c70fb2a2f2ee639
pidaraks1488 <pidaraks1488@gmail.com>
2026-09-05 14:26:24 +0200

    Add label UI component to StarterGui

---
+++ ui/basic/label.lua
@@ -0,0 +1,35 @@
+local G2L = {};
+
+-- StarterGui.label
+G2L["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
+G2L["1"]["Name"] = [[label]];
+G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
+
+
+-- StarterGui.label.label
+G2L["2"] = Instance.new("Frame", G2L["1"]);
+G2L["2"]["BorderSizePixel"] = 0;
+G2L["2"]["BackgroundColor3"] = Color3.fromRGB(44, 44, 44);
+G2L["2"]["Size"] = UDim2.new(0, 150, 0, 50);
+G2L["2"]["Position"] = UDim2.new(0.43523, 0, 0.12734, 0);
+G2L["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["2"]["Name"] = [[label]];
+
+
+-- StarterGui.label.label.TextLabel
+G2L["3"] = Instance.new("TextLabel", G2L["2"]);
+G2L["3"]["TextWrapped"] = true;
+G2L["3"]["BorderSizePixel"] = 0;
+G2L["3"]["TextSize"] = 14;
+G2L["3"]["TextScaled"] = true;
+G2L["3"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["3"]["FontFace"] = Font.new([[rbxasset://fonts/families/GothamSSm.json]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+G2L["3"]["TextColor3"] = Color3.fromRGB(228, 228, 228);
+G2L["3"]["BackgroundTransparency"] = 1;
+G2L["3"]["Size"] = UDim2.new(0, 122, 0, 30);
+G2L["3"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["3"]["Position"] = UDim2.new(0.09333, 0, 0.2, 0);
+
+
+
+return G2L["1"], require;
